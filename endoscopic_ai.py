afbad36cb3fc2de31665fc3b0a7f065b7e6564a0
Taichi Endoh <88502617+TaichiEndoh@users.noreply.github.com>
2021-08-27 09:52:10 +0900

    Update endoscopic_ai.py

--- endoscopic_ai.py
+++ endoscopic_ai.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-%matplotlib inline
+#%matplotlib inline
 import codecs
 import lightgbm as lgb
 from sklearn.model_selection import StratifiedShuffleSplit
